1978ec20eb1372d07aeb8cfd92a39ec0228b234c
ximena-p <73796581+ximena-p@users.noreply.github.com>
2021-02-04 13:27:48 -0500

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,10 +5,10 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-int n;
+	int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
 }
