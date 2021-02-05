6eca5dd657886a4b984a23c6c446d0b931491dc0
Ximena <ximeartista15@gmail,com>
2021-02-05 02:39:25 +0000

    Finish task 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int i;
+int j;
+
+for (i = 48; i <= 57; i++)
+{
+for (j = 48; j <= 57; j++)
+{
+putchar(i);
+putchar(j);
+if (i < 57 || j < 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+
+return (0);
+}
