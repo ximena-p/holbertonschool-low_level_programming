e6fd2eab2753eaaf6f2b6f56da56affddb74dd67
Ximena <ximeartista15@gmail,com>
2021-02-05 02:48:18 +0000

    task point 100 101 102

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int i;
+int j;
+
+for (i = 48; i <= 57; i++)
+{
+for (j = i + 1; j <= 57; j++)
+{
+putchar(i);
+putchar(j);
+if (i < 56 || j < 57)
+{
+putchar(',');
+putchar(' ');
+}
+       
+}
+putchar('\n');
+
+return (0);
+}
