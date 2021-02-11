cbee485d8d6846048bf095a4b151dce64a531dd8
Ximena Perez <ximeartista15@gmail.com>
2021-02-11 20:28:35 +0000

    Print numbers 1 - 9

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: number one
+ * @b: number two
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - print numbers 1 - 9
+ *
+ * Return: 0 (Successs)
+ *
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 47; n < 58; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
